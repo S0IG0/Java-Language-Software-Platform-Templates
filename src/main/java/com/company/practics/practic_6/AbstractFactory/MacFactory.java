04d7c6449938cae03f4bdfb83fcf50d239cc19e9
Даниил <dchibitok@yandex.ru>
2023-02-17 01:54:36 +0300

    added practic_6

---
+++ src/main/java/com/company/practics/practic_6/AbstractFactory/MacFactory.java
@@ -0,0 +1,13 @@
+package main.java.com.company.practics.practic_6.AbstractFactory;
+
+public class MacFactory implements GUIFactory {
+    @Override
+    public Button createButton() {
+        return new MacButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new MacCheckbox();
+    }
+}
