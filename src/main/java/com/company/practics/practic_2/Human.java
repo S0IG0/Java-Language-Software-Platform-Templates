2922a0f568ad2a23eedab697ec1712795151bc3e
Даниил <dchibitok@yandex.ru>
2023-02-17 01:51:16 +0300

    added practic_2

---
+++ src/main/java/com/company/practics/practic_2/Human.java
@@ -0,0 +1,6 @@
+package main.java.com.company.practics.practic_2;
+
+import java.time.LocalDate;
+
+public record Human (int age, String firstName, String lastName, LocalDate birthDate, int weight) {
+}
