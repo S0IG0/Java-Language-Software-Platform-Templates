425dce14bd69379999f822dfcb083ba77a8c660b
Даниил <dchibitok@yandex.ru>
2023-02-17 01:50:51 +0300

    added practic_1

---
+++ src/main/java/com/company/practics/practic_1/EmailValidator.java
@@ -0,0 +1,16 @@
+package main.java.com.company.practics.practic_1;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements Predicate<String> {
+
+    private static final Pattern EMAIL_REGEX = Pattern.compile(
+            "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$", Pattern.CASE_INSENSITIVE
+    );
+
+    @Override
+    public boolean test(String email) {
+        return EMAIL_REGEX.matcher(email).matches();
+    }
+}
